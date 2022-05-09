f7e34f09321510706e1973861ce322dbcc52dafd
Ales Cernivec <ales.cernivec@xlab.si>
2022-05-09 12:00:32 +0200

    remove: workflows_api calls from the collector, updated alerts stream thread with workflowsinfos

--- app/models/manageiq/providers/cisco_intersight/physical_infra_manager/event_catcher/stream.rb
+++ app/models/manageiq/providers/cisco_intersight/physical_infra_manager/event_catcher/stream.rb
@@ -21,7 +21,7 @@ module ManageIQ::Providers::CiscoIntersight
     end
 
     def poll
-      since = Time.new(2000).utc.iso8601
+      since = Time.new(2000).utc.iso8601    ## Fetching all Alerts/Workflow infos since the year 2000. Should be part of config?
       loop do
         @ems.with_provider_connection do |api_client|
           catch(:stop_polling) do
@@ -31,6 +31,7 @@ module ManageIQ::Providers::CiscoIntersight
             events = IntersightClient::CondApi.new(api_client).get_cond_alarm_list(@cond_api_opts).results
             @workflow_api_opts = {
               filter: 'CreateTime gt '+since.to_s,
+              select: '$select=CreateTime,ModTime,Name,Status,Email,WorkflowCtx', # Use only selected attributes. Validation of other within the SDK lib might fail
             }
             workflow_infos = IntersightClient::WorkflowApi.new(api_client).get_workflow_workflow_info_list(@workflow_api_opts).results
             since = Time.now.utc.iso8601
